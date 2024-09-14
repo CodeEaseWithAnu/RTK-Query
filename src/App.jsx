ebce5c5373b9c04f9eac3e215c371b4e30171dbf
CodeEaseWithAnu <ganumols@gmail.com>
2024-09-14 12:03:15 +0530

    RTK Query-fetch

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Allproducts from './component/Allproducts'
+
+const App = () => {
+  return (
+    <div>
+      <Allproducts></Allproducts>
+    </div>
+  )
+}
+
+export default App
